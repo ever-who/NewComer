76c9ef7b12fbde67c88f0e005c0b5cb92012952b
ever_who <tomorrow94i@163.com>
2019-10-28 22:42:29 +0800

    taobao-1111-coupon-script

--- python/taobao-11-repeat.py
+++ python/taobao-11-repeat.py
@@ -5,22 +5,26 @@ import sys, os, time, random
 # repeat operation 
 #      ---  coordination for xiaomi MIX2S
 ###################################################
-def jd_gold_coin_repeat(num):
+def taobao_coin_repeat(num):
 	os.system('adb devices')
 	for i in range(1,num):
 		print(str(i)+" start")
-		time.sleep(random.randint(3,5))
-		os.system('adb shell input tap 923 895')#1333
+		time.sleep(random.randint(2,4))
+		os.system('adb shell input tap 906 1842')#1333
 		time.sleep(random.randint(6,9))
+		count=0
+		for j in range(1,random.randint(3,4)):
+			os.system('adb shell input roll 0 3')
+			time.sleep(random.randint(3,5))
+			count=+1
+		time.sleep(random.randint(16-count*3,18-count*3))
 		os.system('adb shell input keyevent 4')
-		time.sleep(random.randint(3,5))
-		os.system('adb shell input tap 565 1475')
 
 # Main Script
 ###################################################
 if __name__ == '__main__':
 	num=int(sys.argv[1])+1
-	jd_gold_coin_repeat(num)
+	taobao_coin_repeat(num)
 
 ###################################################
 # End of File
