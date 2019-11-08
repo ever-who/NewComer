ae8f6cc316be1c98465076eb74d304752ac52c61
ever_who <tomorrow94i@163.com>
2019-11-08 21:30:44 +0800

    taobao-1111-modify

--- python/taobao-11-repeat.py
+++ python/taobao-11-repeat.py
@@ -3,14 +3,21 @@ import sys, os, time, random
 
 ###################################################
 # repeat operation 
-#      ---  coordination for xiaomi MIX2S
+#      ---  coordination for xiaomi MIX2S 
+#  店铺button (906,1842);会场button (891,1294);
+#  order : 0 -- 店铺 ， 1 -- 会场
 ###################################################
-def taobao_coin_repeat(num):
-	os.system('adb devices')
+def taobao_coin_repeat(num,order):
+	if num <=1 :
+		return
 	for i in range(1,num):
-		print(str(i)+" start")
 		time.sleep(random.randint(2,4))
-		os.system('adb shell input tap 906 1842')#1333
+		if order == 0:
+			print(str(i)+" shop start")
+			os.system('adb shell input tap 906 1842')# shop
+		elif order ==1:
+			print(str(i)+" hall start")
+			os.system('adb shell input tap 891 1294')# hall
 		time.sleep(random.randint(6,9))
 		count=0
 		for j in range(1,random.randint(3,4)):
@@ -23,8 +30,11 @@ def taobao_coin_repeat(num):
 # Main Script
 ###################################################
 if __name__ == '__main__':
-	num=int(sys.argv[1])+1
-	taobao_coin_repeat(num)
+	shop_num=int(sys.argv[1])+1
+	hall_num= int(sys.argv[2])+1
+	os.system('adb devices')
+	taobao_coin_repeat(shop_num,0)
+	taobao_coin_repeat(hall_num,1)
 
 ###################################################
 # End of File
