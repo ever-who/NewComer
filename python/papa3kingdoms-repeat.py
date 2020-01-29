ea2734055c79d0d6a9d8758bf994996dc6c30090
ever_who <tomorrow94i@163.com>
2020-01-29 21:38:40 +0800

    add papasanguo 2020 new year scripts

--- python/papa3kingdoms-repeat.py
+++ python/papa3kingdoms-repeat.py
@@ -42,7 +42,33 @@ def papa_3_gold_card(num):
 		time.sleep(random.randint(15,20))
 		os.system('adb shell input tap 1530 687')#继续解锁
 		time.sleep(random.randint(5,8))
-
+		
+		
+###################################################
+# repeat operation 2 武将擂台
+#      ---  coordination for xiaomi MIX2S
+###################################################
+def papa_3_new_year(num):
+	os.system('adb devices')
+	for i in range(1,num):
+		print(str(i)+" start")
+		time.sleep(random.randint(2,4))
+		os.system('adb shell input tap 851 534')#化敌为友
+		time.sleep(random.randint(2,4))
+		os.system('adb shell input tap 1550 644')#征战
+		time.sleep(random.randint(6,8))
+		os.system('adb shell input tap 2045 916')#开战
+		time.sleep(random.randint(3,5))
+		os.system('adb shell input tap 1562 950')#跳过
+		time.sleep(random.randint(95,100))
+		os.system('adb shell input tap 1109 926')#退出
+		time.sleep(random.randint(5,10))
+		os.system('adb shell input tap 851 534')#抽卡
+		time.sleep(random.randint(5,8))
+		os.system('adb shell input tap 851 534')#抽卡
+		time.sleep(random.randint(5,8))
+		os.system('adb shell input tap 1100 940')#继续
+		time.sleep(random.randint(5,8))
 # Main Script
 ###################################################
 if __name__ == '__main__':
@@ -51,6 +77,8 @@ if __name__ == '__main__':
 		papa_3_gold_card(num)
 	elif sys.argv[1]=="-w":
 		papa_3_workshop(num)
+	elif sys.argv[1]=="-y":
+		papa_3_new_year(num)
 
 ###################################################
 # End of File
